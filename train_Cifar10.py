558b0790317f9f1f431c6209df0a1d7d639ea6d3
Vladimir Khomyakov <rozendetr@bk.ru>
2020-04-23 10:03:21 +0300

    init train for cifar10 and init utils

---
+++ train_Cifar10.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+import argparse
+
+from models import *
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true',
+                    help='resume from checkpoint')
+args = parser.parse_args()
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print('select device: ', device)
+
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+# Data
+print('==> Preparing data..')
+
+means = (0.4914, 0.4822, 0.4465)
+deviations = (0.2023, 0.1994, 0.2010)
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(means, deviations),
+    ])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(means, deviations),
+    ])
+
+
+
---
+++ utils.py
@@ -0,0 +1,9 @@
+import os
+import sys
+import time
+import math
+import torch
+
+import torch.nn as nn
+import torch.nn.init as init
+
