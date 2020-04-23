c62eba594631b3bb01052b5db5f7e3ec55829cd9
Vladimir Khomyakov <rozendetr@bk.ru>
2020-04-23 10:38:21 +0300

    init train fashion_mnist

--- train_FashionMnist.py
+++ train_FashionMnist.py
@@ -27,26 +27,29 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 
-means = [0.4914, 0.4822, 0.4465]
-deviations = [0.2023, 0.1994, 0.2010]
+means = [0.5]
+deviations = [0.5]
 
 transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
+    transforms.RandomCrop(28, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize(means, deviations),
+    transforms.Lambda(lambda x: x.repeat(3, 1, 1)),  # fashionMNIST have grayscale image, convert grayscale to "color"
+                                                    # just repeat 1 layer to 3 layer
     ])
 
 transform_test = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(means, deviations),
+    transforms.Lambda(lambda x: x.repeat(3, 1, 1) ),
     ])
 
-trainset = torchvision.datasets.CIFAR10(
+trainset = torchvision.datasets.FashionMNIST(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=0)
-testset = torchvision.datasets.CIFAR10(
+testset = torchvision.datasets.FashionMNIST(
     root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=0)
